e094a3b0772f6cd8a42e453151c4bc84e50123ae
IngridFerreira93 <ingrid.b.f@hotmail.com>
2021-05-20 21:04:15 -0300

    atividade e desafio

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,56 @@
+/* Exercícios de interpretação de código 
+1. aparece o numero 10 e o 5 no console.*/
+
+/* 
+2. Vai aparecer o 10 3 vezes.  */
+
+/* 3. p por horasDia 
+      t por valorDia */
+
+
+/*Exercícios de escrita de código 
+1.
+
+const nome = prompt("Qual o seu nome?")
+const idade = prompt("Qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)
+*/ //Como não usamos o (Number) na idade ele vem como string.
+
+/*2.
+let trabalhoTecnologia = prompt("Você trabalha com Tecnologia?")
+let gosto = prompt("Você gosta da area?")
+let houveIndicacao = prompt("Houve alguma indicação?")
+console.log("Olá, você trabalha com tecnologia?", trabalhoTecnologia, "você gosta da area?",
+    gosto,
+    "Houve alguma indicação?", houveIndicacao) */
+
+/*3.
+
+let a = 10
+let b = 25
+let c = a
+a = b
+b = c
+
+
+// Aqui faremos uma lógica para trocar os valores
+// Depois de trocados, teremos o seguinte resultado:
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10 */
+
+
+/*  DESAFIO
+
+1. O primeiro número somado ao segundo número resulta em: x.
+2. O primeiro número multiplicado pelo segundo número resulta em: y.
+
+let primeiroNumero = Number(prompt("Escreva um número"))
+let segundoNumero = Number(prompt("Escreva mais um numero"))
+
+let primeiroResultado = primeiroNumero + segundoNumero
+let segundoResultado = primeiroNumero * segundoNumero
+
+console.log("O", primeiroNumero, "somado ao", segundoNumero, "resulta em", primeiroResultado)
+console.log("O", primeiroNumero, "mutiplicado pelo", segundoNumero, "resulta em", segundoResultado) */
