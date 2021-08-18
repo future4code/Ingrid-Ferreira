2b746647fb9863a965d0ccaa3219ea4494c7cdf4
IngridFerreira93 <ingrid.b.f@hotmail.com>
2021-08-18 10:57:55 -0300

    comeco pagina App.js só elementos

--- quinzena7/projeto-labefy/src/App.js
+++ quinzena7/projeto-labefy/src/App.js
@@ -17,7 +17,17 @@ display: flex;
 font-size: 35px;
 align-items: center;
 `;
+const BtnPaginas = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
 
+  button {
+    border: none;
+    background-color: #FAB24E;
+    font-size: 30px;
+  }
+`
 const Footer = styled.footer`
 background-color: #FE7E01;
 display: flex;
@@ -25,12 +35,39 @@ justify-content: flex-end;
 padding: 25px;
 `;
 
+const TextoRodape = styled.h4`
+  margin-left: 20px;
+`;
+
 export default class App extends React.Component {
+
+  state ={
+    playlists: [],
+    inputPlaylist: "",
+    paginaAtual: "cadastro",
+    playlistDetalhe: "",
+    playlistDetalheNome: "",
+    playlistDetalheId: "",
+    inputMusica: "",
+    inputArtistaMusica: "",
+    inputUrlMusica: ""
+  }
+
+
+
+  
   render() {
     return (
       <Site>
         <Header>Labefy</Header>
-        <Footer></Footer>
+                
+          <BtnPaginas><button onClick={this.goToPaginaLista}>Lista de Playlists</button></BtnPaginas>
+          <BtnPaginas><button onClick={this.goToPaginaCadastro}>Cadastrar Playlists</button></BtnPaginas>
+          <BtnPaginas><button onClick={this.goToPaginaLista}>Voltar</button></BtnPaginas>
+        <Footer>
+          <p class="text">©️ 2021 All rights reserved.</p>
+          <TextoRodape>Desenvolvido pela estudante da Turma Johnson: Ingrid Ferreira</TextoRodape>
+        </Footer>
       </Site>
 
       
